7510a5c1e880d3c6358861602c5eed8a60bc824d
Kushal Pandya <kushalspandya@gmail.com>
2019-11-26 17:04:18 +0530

    Update script to fence GUI apps installation

--- brew.sh
+++ brew.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
 # Check Xcode CLI tools presence, install if doesn't exist
-if [[ $(xcode-select --version) ]]; then
-  echo "Xcode command tools already installed"
-else
-  echo "Installing Xcode commandline tools"
-  $(xcode-select --install)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if [[ $(xcode-select --version) ]]; then
+    echo "Xcode command tools already installed"
+  else
+    echo "Installing Xcode commandline tools"
+    $(xcode-select --install)
+  fi
 fi
 
 # Check Homebrew presence, install if doesn't exist
 if [[ $(brew --version) ]] ; then
-    echo "Attempting to update Homebrew"
-    brew update
-    brew upgrade
-    brew cask upgrade
+  echo "Attempting to update Homebrew"
+  brew update
+  brew upgrade
+  brew cask upgrade
 else
-    echo "Attempting to install Homebrew"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo "Attempting to install Homebrew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Begin Homebrew package installation
-echo Effective Homebrew version:
+echo "Effective Homebrew version:"
 brew --version
 
 # Save Homebrew’s installed location.
@@ -31,15 +33,12 @@ BREW_PREFIX=$(brew --prefix)
 brew install coreutils
 ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
-# Install some other useful utilities like `sponge`.
+# Install GNU & additional CLI utilities
 brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 
 # Install system & networking tools
-brew install neofetch
 brew install wget --with-iri
 brew install grep
 brew install openssh
@@ -60,32 +59,54 @@ brew install go
 brew install node
 brew install yarn
 
-# GUI Apps
-brew cask install google-chrome
-brew cask install firefox
-brew cask install opera
-brew cask install google-backup-and-sync
-brew cask install dropbox
-brew cask install iterm2
-brew cask install visual-studio-code
-brew cask install atom
-brew cask install macpass
-brew cask install gimp
-brew cask install libreoffice
-brew cask install zoomus
-brew cask install vlc
-brew cask install teamviewer
-brew cask install numi
-brew cask install slack
-brew cask install spotify
+# Extra CLI utilities
+brew install bat
+brew install youtube-dl
 
-# Fonts
-brew tap homebrew/cask-fonts
-brew cask install font-fira-code
-brew cask install font-cascadia
-brew cask install font-source-code-pro
-brew cask install font-source-sans-pro
-brew cask install font-source-serif-pro
+# GUI Apps (macOS only)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "Attempting to install GUI apps"
+  ## Productivity
+  brew cask install the-unarchiver
+  brew cask install iterm2
+  brew cask install gimp
+  brew cask install libreoffice
+  brew cask install numi
+  brew cask install pencil
+  brew cask install macpass
+  brew cask install teamviewer
+
+  ## Web Browsers
+  brew cask install google-chrome
+  brew cask install chromedriver
+  brew cask install firefox
+  brew cask install opera
+
+  ## Cloud Storage Providers
+  brew cask install google-backup-and-sync
+  brew cask install dropbox
+
+  ## Text Editors
+  brew cask install visual-studio-code
+  brew cask install atom
+  brew cask install sublime-text
+
+  ## Multimedia
+  brew cask install vlc
+  brew cask install spotify
+
+  ## Work Tools
+  brew cask install zoomus
+  brew cask install slack
+
+  # Fonts
+  brew tap homebrew/cask-fonts
+  brew cask install font-fira-code
+  brew cask install font-cascadia
+  brew cask install font-source-code-pro
+  brew cask install font-source-sans-pro
+  brew cask install font-source-serif-pro
+fi
 
 # GitLab Development Kit tools
 brew install redis
@@ -100,4 +121,3 @@ brew install gpg
 brew install runit
 brew install icu4c
 brew install exiftool
-brew cask install chromedriver
