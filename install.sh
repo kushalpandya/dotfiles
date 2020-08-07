fdcacdb8a0c04b06dd1e5a95379990f817f2d25e
Kushal Pandya <kushalspandya@gmail.com>
2020-08-07 13:40:38 +0530

    Update to include new scripts

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-# Setup Runtime for Script
+# Setup Runtime variables for script
+export BOLD=$(tput bold) # Bold
+export NORMAL=$(tput sgr0) # Regular
+export NC='\033[0m' # No color
 
 # An echo with custom color!
 # derived from https://stackoverflow.com/a/5947802/414749
 echo_e() {
   BLUE='\033[1;34m' # Light Blue Color
-  NC='\033[0m' # No color
 
-  echo -e "${BLUE}----------$1---------${NC}"
+  echo -e "${BOLD}${BLUE}----------$1---------${NC}${NORMAL}"
+}
+
+echo_q() {
+  RED='\033[1;31m' # Light Red Color
+
+  echo -e -n "${BOLD}${RED}$1${NC}${NORMAL}"
 }
 
 # Installation begins here.
@@ -19,21 +27,45 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   chmod +x $PWD/macOS/brew.sh
   chmod +x $PWD/macOS/zsh.sh
 
-  echo "Running Homebrew"
+  echo_e "Running Homebrew for non-GUI packages"
   ./macOS/brew.sh
+
+  # Prompt for GUI package installations
+  echo_q "Do you want to run Homebrew Cask for GUI apps? [y/n]"
+  read answer
+  if [ "$answer" != "${answer#[Yy]}" ]; then
+    ./macOS/brew-cask.sh
+  else
+    echo_e "Skipped GUI packages installation"
+  fi
 
   echo "Setting up Zsh"
   ./macOS/zsh.sh
 else
   echo_e "Setting up script permissions"
-  chmod +x $PWD/Ubuntu/apt.sh
+  chmod +x $PWD/Ubuntu/apt-cli.sh
+  chmod +x $PWD/Ubuntu/apt-gui.sh
   chmod +x $PWD/Ubuntu/brew.sh
   chmod +x $PWD/Ubuntu/zsh.sh
 
-  echo_e "Running Apt"
-  ./Ubuntu/apt.sh
+  echo_e "Making sure there are no pending upgrades"
+  sudo apt update && sudo apt upgrade -y && sudo apt autoremove && sudo apt autoclean
 
-  echo_e "Running Homebrew"
+  echo_e "Running apt for non-GUI packages"
+  ./Ubuntu/apt-cli.sh
+
+  # Prompt for GUI package installations
+  echo_q "Do you want to run apt for GUI packages? [y/n]"
+  read answer
+  if [ "$answer" != "${answer#[Yy]}" ]; then
+    sudo -E ./Ubuntu/apt-gui.sh
+  else
+    echo_e "Skipped GUI packages installation"
+  fi
+  echo_e "Running post-install apt cleanup"
+  sudo apt autoremove && sudo apt autoclean
+
+  echo_e "Running Homebrew for non-GUI packages"
   ./Ubuntu/brew.sh
 
   echo_e "Setting up Zsh"
