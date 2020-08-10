71da0e2de046595af5f067621e19bf5dae7d0be4
Kushal Pandya <kushalspandya@gmail.com>
2020-08-10 15:38:06 +0530

    Move out common commands to common/zsh.sh

--- Ubuntu/zsh.sh
+++ Ubuntu/zsh.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Installing and configuring Zsh"
+if [[ $(zsh --version) != '' ]]; then
+  # Switch to using Zsh as default shell
+  chsh -s `which zsh`
+
+  # Copy Alias file
+  cp $PWD/zsh/Ubuntu/aliases.zsh ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/
+
+  # Copy .zshrc
+  cp $PWD/zsh/Ubuntu/zshrc ~/.zshrc
+
+  source ~/.zshrc
+fi
--- macOS/zsh.sh
+++ macOS/zsh.sh
@@ -1,34 +1,14 @@
 #!/usr/bin/env bash
 
-# Check Zsh presence, install if doesn't exist
-if [[ $(zsh --version) ]]; then
-  echo "Zsh already installed"
-else
-  echo "Attempting to install Zsh..."
-  brew install zsh
+if [[ $(zsh --version) != '' ]]; then
+  # Switch to using Zsh as default shell
+  chsh -s `which zsh`
+
+  # Copy Alias file
+  cp $PWD/zsh/macOS/aliases.zsh ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/
+
+  # Copy .zshrc
+  cp $PWD/zsh/macOS/zshrc ~/.zshrc
+
+  source ~/.zshrc
 fi
-
-# Switch to using Zsh as default shell
-echo "Making Zsh default shell..."
-chsh -s `which zsh`
-
-# Install Oh-my-zsh
-echo "Installing Oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-
-# Install Zsh Plugins
-echo "Installing Oh-my-zsh plugins..."
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# Copy Avit-arrow theme
-cp $PWD/common/avit-arrow.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes
-
-# Copy Alias file
-cp $PWD/zsh/macOS/aliases.zsh ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/
-
-# Copy .zshrc
-cp $PWD/zsh/macOS/zshrc ~/.zshrc
-
-source ~/.zshrc
