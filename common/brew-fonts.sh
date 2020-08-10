d309bfc3f50d0d00850c0b1884e41411b260de6e
Kushal Pandya <kushalspandya@gmail.com>
2020-08-10 15:35:55 +0530

    Fonts Install Script

---
+++ common/brew-fonts.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Setup OS-specific install command alias
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS; use `brew cask`
+  alias install_font="brew cask install"
+else
+  # Linux; use `brew`
+  alias install_font="brew install"
+fi
+
+# Fonts
+install_font font-jetbrains-mono
+install_font font-jetbrains-mono-nerd-font
+install_font font-fira-code
+install_font font-cascadia
+install_font font-source-code-pro
+install_font font-source-sans-pro
+install_font font-source-serif-pro
