f1bf8b6979d2c727875e79d65a3d2ed6a82d4b52
Kushal Pandya <kushalspandya@gmail.com>
2021-02-12 17:50:20 +0530

    Add nodejs and yarn plugins

--- common/asdf-plugins.sh
+++ common/asdf-plugins.sh
@@ -10,5 +10,7 @@ else
 fi
 
 # Add Ruby & Go plugins
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add yarn https://github.com/twuni/asdf-yarn.git
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
